bad97777ba14ec04fb517dde681e4148bda6cfb7
Maxime <richard.maxime9@gmail.com>
2016-10-04 22:20:25 +0200

    Mise en place action bouton pagination

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -9,30 +9,31 @@ class Carousel extends Component {
             index: 0,
             direction: 'right'
         };
+
     };
 
     next() {
         let nextSlide = this.state.index + 1 < this.props.imgs.length ? this.state.index + 1 : 0;
         this.setState({ index: nextSlide, direction: 'right' });
-    }
+    };
 
     prev() {
         var prevSlide = this.state.index - 1 < 0 ? this.props.imgs.length - 1 : this.state.index - 1;
         this.setState({ index: prevSlide, direction: 'left' });
-    }
+    };
 
-    goto(indexButton) {
-
-    }
+    goto(indexButton)  {
+        let indexCourant = this.state.index 
+        let direction = (indexButton > indexCourant) ? 'right' : 'left' 
+        this.setState({ index: indexButton, direction: direction });
+    };
 
     render() {
-
         let indexCourant = this.state.index 
-        
         let buttonsPagination = this.props.imgs.map(function (img, i) {
             let buttonActive = (indexCourant === i) ? 'active' : ''
             return (<button key={i} onClick={() => this.goto(i)} className={buttonActive}></button>)
-        })
+        }.bind(this))
 
         return (
             <div className="carousel" >
